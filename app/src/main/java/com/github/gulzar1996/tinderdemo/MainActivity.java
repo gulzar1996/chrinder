917304954234b61953eaaeda51fa856c04778acd
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-11-11 09:31:30 +0530

    final commit

--- app/src/main/java/com/github/gulzar1996/tinderdemo/MainActivity.java
+++ app/src/main/java/com/github/gulzar1996/tinderdemo/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mUser = new ArrayList<>();
         for (int i=0;i<50; i++){
         mUser.add(new User("Not Emilia Clarke","24","Hunting","https://timedotcom.files.wordpress.com/2017/06/emilia-clarke-quiz.jpg"));
-        mUser.add(new User("Random","21","Sleeping","http://photogallery.indiatimes.com/photo/59472687.cms"));
+        mUser.add(new User("Vicky Donor","21","Sleeping","https://pbs.twimg.com/media/C3pLaVuUkAAw87n.jpg"));
         mUser.add(new User("Katy Perry","28","Singing","https://media.vanityfair.com/photos/58c815f89a1bb337a29dbe43/master/w_768,c_limit/Katy-Perry-Rainbow-Hair-SS-16.jpg"));
         mUser.add(new User("Shradha","21","Actor","https://s-media-cache-ak0.pinimg.com/736x/2c/da/47/2cda4706ab1bd42c46962cd3cfc27356.jpg"));}
 
